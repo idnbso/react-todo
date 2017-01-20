fcd4572587c954ef942e23d49aeeddd6546935c1
Idan <idanbusso@gmail.com>
2017-01-20 18:54:43 +0200

    Added toggle support

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -12,19 +12,23 @@ let TodoApp = React.createClass({
             todos: [
                 {
                     id: uuid(),
-                    text: "Walk the dog."
+                    text: "Walk the dog.",
+                    completed: false
                 },
                 {
                     id: uuid(),
-                    text: "Clean the yard."
+                    text: "Clean the yard.",
+                    completed: true
                 },
                 {
                     id: uuid(),
-                    text: "Feed the cat."
+                    text: "Feed the cat.",
+                    completed: true
                 },
                 {
                     id: uuid(),
-                    text: "Clean your room."
+                    text: "Clean your room.",
+                    completed: false
                 }
             ]
         };
@@ -35,10 +39,22 @@ let TodoApp = React.createClass({
                 ...this.state.todos,
                 {
                     id: uuid(),
-                    text: text
+                    text: text,
+                    completed: false
                 }
             ]
         });
+    },
+    handleToggle: function(id) {
+        let updatedTodos = this.state.todos.map((todo) => {
+            if (todo.id === id) {
+                todo.completed = !todo.completed;
+            }
+
+            return todo;
+        });
+
+        this.setState({todos: updatedTodos});
     },
     handleSearch: function(showCompleted, searchText) {
         this.setState({
@@ -52,7 +68,7 @@ let TodoApp = React.createClass({
         return (
             <div>
                 <TodoSearch onSearch={this.handleSearch}/>
-                <TodoList todos={todos}/>
+                <TodoList todos={todos} onToggle={this.handleToggle}/>
                 <AddTodo onAddTodo={this.handleAddTodo}/>
             </div>
         )
--- app/tests/components/Todo.test.jsx
+++ app/tests/components/Todo.test.jsx
@@ -10,4 +10,20 @@ describe('Todo', () => {
     it('should exist', () => {
         expect(Todo).toExist();
     });
+
+    it('should call onToggle prop with id on click', () => {
+        const todoData = {
+            id: 199,
+            text: 'Write todo.test.jsx test',
+            completed: true
+        };
+
+        const spy = expect.createSpy();
+        let todo = TestUtils.renderIntoDocument(<Todo {...todoData} onToggle={spy}/>);
+        let $el = $(ReactDOM.findDOMNode(todo));
+
+        TestUtils.Simulate.click($el[0]);
+
+        expect(spy).toHaveBeenCalledWith(todoData.id);
+    });
 });
